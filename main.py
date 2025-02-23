342c8c2ebae22c8890c830b585f00b842be688a7
Karina Quiroga <iquiroga5@estudiantes.areandina.edu.co>
2025-02-22 22:22:12 -0500

    Hola

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from views.transaction_type import TransactionTypeUI
 import tkinter as tk
-# Hola mundo
+# Hola mundo2
 if __name__ == "__main__":
     root = tk.Tk()
     app = TransactionTypeUI(root)
