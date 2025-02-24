42c243e079a0e006ba427c5ad8e3531b21695052
Diego Murillo <diegoa31052004@gmail.com>
2025-02-23 19:05:30 -0500

    feat: Index to move to different views

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from views.transaction_type import TransactionTypeUI
 import tkinter as tk
+from views.index import IndexIu
+from views.transaction_type import TransactionTypeUI
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Sistema de Gestión")
+        self.geometry("400x300")
+
+        # Contenedor de vistas
+        self.container = tk.Frame(self)
+        self.container.pack(fill="both", expand=True)
+
+        # Diccionario de vistas
+        self.frames = {}
+
+        # Agregar las vistas a la aplicación
+        for F in (IndexIu, TransactionTypeUI):
+            frame = F(self.container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(IndexIu)
+
+    def show_frame(self, cont):
+        """Muestra el frame dado en `cont`."""
+        frame = self.frames[cont]
+        frame.tkraise()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = TransactionTypeUI(root)
-    root.mainloop()
+    app = App()
+    app.mainloop()
